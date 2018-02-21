1b12c60d7bd53dc93d01417d8264b3390249fe44
Danielalab <olabolacrayola@hotmail.com>
2018-02-20 23:26:34 -0500

    funcionalidad ajax api uber

--- public/js/app.js
+++ public/js/app.js
@@ -14,6 +14,8 @@ firebase.initializeApp(config);
 let inputFrom = document.getElementById('inputFrom');
 let inputTo = document.getElementById('inputTo');
 let btnRoad = document.getElementById('search-road');
+let tokenServerApi = 'XlaqCTZjMlVmadQPgMnuAANm3W-uQG911SVpYmba';
+let distance;
 
 // funcionalidad para el autocompletado de inputs
 let autocompleteInputs = () => {
@@ -30,23 +32,58 @@ let calculateAndDisplayRoute = (directionsService, directionsDisplay) => {
   }, function(response, status) {
     if (status === 'OK') {
       directionsDisplay.setDirections(response);
+      let startLongitude = response.routes[0].bounds.b.b;
+      let endLongitude = response.routes[0].bounds.b.f;
+      let startLatitude = response.routes[0].bounds.f.b;
+      let endLatitude = response.routes[0].bounds.f.f; 
+      estimatePricesRoute(startLatitude, startLongitude, endLatitude, endLongitude);
     } else {
       window.alert('Directions request failed due to ' + status);
     }
   });
 };
 
+// funcionalidad para estimar el precio de la ruta con API UBER
+let succes = (data) => {
+  console.log(data);
+  console.log('estoy en succes');
+};
+
+let handleError = () => {
+  console.log('error');
+};
+
+let estimatePricesRoute = (startLatitude, startLongitude, endLatitude, endLongitude) => {
+  let proxy = 'https://cors-anywhere.herokuapp.com/';
+  let apiLink = 'https://api.uber.com/v1/estimates/price';
+  $.ajax({
+    url: proxy + apiLink,
+    headers: {
+      Authorization: 'Token ' + tokenServerApi,
+    },
+    data: {
+      start_latitude: startLatitude,
+      start_longitude: startLongitude,
+      end_latitude: endLatitude,
+      end_longitude: endLongitude
+    },
+    ContentType: 'application/json',               
+  })
+    .done(succes)
+    .fail(handleError);
+};
+
 function initMap() {
   let directionsService = new google.maps.DirectionsService;
   let directionsDisplay = new google.maps.DirectionsRenderer;
   let map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 13,
+    zoom: 12,
     center: {
-      lat: -12.1191427, 
+      lat: -12.1191427,
       lng: -77.0349046,
     }
   });
-  
+
   directionsDisplay.setMap(map);
   let initRoad = (event) => {
     event.preventDefault();
@@ -56,6 +93,3 @@ function initMap() {
   // asociando evento a elemento del DOM 
   btnRoad.addEventListener('click', initRoad);
 }
-
-// uEDUdI3cnJay7F8jd9WbsOAVolKhN9HI ID
-// TOKEN XlaqCTZjMlVmadQPgMnuAANm3W-uQG911SVpYmba
